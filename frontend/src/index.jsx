5f3ae879c68d30f9719e2631eb48f79cfd0b816c
Matthew Lau <matthew.lau78@gmail.com>
2021-04-08 23:47:07 +1000

    notifications are working

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import AlertProvider from './contexts/AlertProvider';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AlertProvider>
+      <App />
+    </AlertProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navigation from '../components/Navigation';
+import { useAlert } from '../contexts/AlertProvider';
 
 const Dashboard = () => {
+  const [inputVal, setInputVal] = useState('');
+  const [inputVal2, setInputVal2] = useState('');
+  const dispatch = useAlert();
+
+  const handleNewAlert = () => {
+    dispatch({
+      type: 'SUCCESS',
+      message: inputVal,
+      title: 'Successful Request'
+    })
+  }
+
+  const handleNewError = () => {
+    dispatch({
+      type: 'ERROR',
+      message: inputVal,
+      title: 'Successful Request'
+    })
+  }
+
   return (
     <>
       <Navigation />
       <p>This is a dashboard</p>
+      <input type="text" value={inputVal} onChange={e => setInputVal(e.target.value)}/>
+      <button onClick={handleNewAlert}>Add Alert</button>
+      <input type="text" value={inputVal2} onChange={e => setInputVal2(e.target.value)}/>
+      <button onClick={handleNewError}>Add Error</button>
+
     </>
   )
 }

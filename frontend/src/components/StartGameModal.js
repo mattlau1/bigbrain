400682d73d3c5a6e1bad4f5ac6a2860f93bf423b
Matthew Lau <matthew.lau78@gmail.com>
2021-04-18 02:13:04 +1000

    finished admin control panel and advance feature

--- frontend/src/components/StartGameModal.js
+++ frontend/src/components/StartGameModal.js
@@ -2,8 +2,9 @@ import React from 'react'
 import { Button, Form, Modal } from 'react-bootstrap'
 import PropTypes from 'prop-types';
 import { useAlert } from '../contexts/AlertProvider';
+import API from '../utils/API';
 
-const StartGameModal = ({ show, handleClose, id }) => {
+const StartGameModal = ({ show, handleClose, sessionId, gameId }) => {
   const dispatch = useAlert();
 
   const createAlert = (type, message) => {
@@ -14,29 +15,52 @@ const StartGameModal = ({ show, handleClose, id }) => {
   }
 
   const handleCopy = (text) => {
-    createAlert('SUCCESS', 'Invitation link copied to clipboard');
-    navigator.clipboard.writeText(`${window.location.origin}/play/${text}`);
+    navigator.clipboard.writeText(text);
+    createAlert('SUCCESS', 'Session ID copied to clipboard');
+  }
+
+  const handleAdvance = async () => {
+    const token = localStorage.getItem('token');
+    const api = new API();
+    try {
+      console.log(gameId)
+      const res = await api.postAPIRequestToken(`admin/quiz/${gameId}/advance`, token);
+      const data = await res.json()
+      if (res.ok) {
+        createAlert('SUCCESS', 'Advanced to the next question');
+      } else {
+        createAlert('ERROR', 'There was a problem advancing to the next question');
+        console.log(data)
+      }
+    } catch (e) {
+      createAlert('ERROR', 'An unexpected error has occurred');
+      console.warn(e);
+    }
   }
 
   return (
-      <Modal show={show} onHide={handleClose}>
+      <Modal show={show} onHide={handleClose} size="lg">
       <Form>
         <Modal.Header closeButton>
         <Modal.Title>Share Game</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <p className="mb-1">Session id: <strong>{id}</strong></p>
+          <p className="mb-1">Your Session ID is...</p>
+          <h1 className="text-center session-id">{sessionId}</h1>
           <Form.Group controlId="inviteLink">
-            <Form.Control
-              type="text"
-              value={`${window.location.origin}/play/${id}`}
-              readOnly
-              plaintext
-            />
-            <Button onClick={() => { handleCopy(id) }}>
-              Copy Link
+            <Button onClick={() => { handleCopy(sessionId) }}>
+              Copy Session ID
             </Button>
           </Form.Group>
+          <p>Join now at <a href={`${window.location.origin}/play/`}>{window.location.origin}/play/</a></p>
+          <hr></hr>
+          <h2 className="text-center">Control Panel</h2>
+          <div className="d-flex justify-content-center align-items-center">
+            <Button variant="success" onClick={() => { handleAdvance() }}>
+              Next Question
+            </Button>
+          </div>
+
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
@@ -49,13 +73,10 @@ const StartGameModal = ({ show, handleClose, id }) => {
 }
 
 StartGameModal.propTypes = {
-  setShow: PropTypes.func,
-  handleShow: PropTypes.func,
   handleClose: PropTypes.func,
   show: PropTypes.bool,
-  gameList: PropTypes.array,
-  setGameList: PropTypes.func,
-  id: PropTypes.number
+  gameId: PropTypes.number,
+  sessionId: PropTypes.number
 };
 
 export default StartGameModal
--- frontend/src/pages/Results.js
+++ frontend/src/pages/Results.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useParams } from 'react-router';
 import Navigation from '../components/Navigation';
+import API from '../utils/API';
 
 const Results = () => {
   const { sessionId } = useParams();
+
+  useEffect(() => {
+    const loadResults = async () => {
+      try {
+        const token = localStorage.getItem('token');
+        const api = new API();
+        const res = await api.getAPIRequestToken(`admin/session/${sessionId}/results`, token);
+        const data = await res.json();
+
+        if (res.ok) {
+          console.log(data);
+        }
+      } catch (e) {
+        console.warn(e);
+      }
+    }
+    loadResults()
+  }, [])
   return (
     <>
       <Navigation />

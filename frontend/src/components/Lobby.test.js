f6921afa122282cc4d443ba82887e49a51614b39
Swxer <swxergamer64@gmail.com>
2021-04-21 03:49:26 +1000

    fixed lint

--- frontend/src/components/Lobby.test.js
+++ frontend/src/components/Lobby.test.js
@@ -1,14 +1,13 @@
-import Enzyme, { shallow } from 'enzyme';
-import React from "react";
-import Lobby from "./Lobby"
+import Enzyme from 'enzyme';
+// import React from "react";
+// import Lobby from "./Lobby"
 import Adapter from 'enzyme-adapter-react-16';
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('Lobby', () => {
-
-    // passing params with generic id
-    it('Render lobby components', () => {
-        const foo = true;
-        expect(foo).toBe(true);
-    })
+  // passing params with generic id
+  it('Render lobby components', () => {
+    const foo = true;
+    expect(foo).toBe(true);
+  })
 })
--- frontend/src/components/Navigation.test.js
+++ frontend/src/components/Navigation.test.js
@@ -1,19 +1,18 @@
 import Enzyme, { mount, shallow } from 'enzyme';
-import React from "react";
-import Navigation from "./Navigation"
+import React from 'react';
+import Navigation from './Navigation'
 import Adapter from 'enzyme-adapter-react-16';
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('Navigation', () => {
+  // get the title of the app
+  it('render navbar components', () => {
+    shallow(<Navigation />);
+  })
 
-    // get the title of the app
-    it('render navbar components', () => {
-        shallow(<Navigation />);
-    })
-
-    it('Get brand title', () => {
-        const wrapper = mount(<Navigation />);
-        const text = wrapper.find('Brand').debug();
-        expect(text.text()).toBe('Big Brain');
-    })
+  it('Get brand title', () => {
+    const wrapper = mount(<Navigation />);
+    const text = wrapper.find('Brand').debug();
+    expect(text.text()).toBe('Big Brain');
+  })
 })

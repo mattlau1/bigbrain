86ead2ded1d93a88b4f3fca27c7af987201b3c16
Matthew Lau <matthew.lau78@gmail.com>
2021-04-22 06:05:50 +1000

    added tree for lobby test

--- frontend/src/components/Lobby.test.js
+++ frontend/src/components/Lobby.test.js
@@ -1,12 +1,15 @@
 import { shallow } from 'enzyme';
 import React from 'react';
 import Lobby from './Lobby';
+import renderer from 'react-test-renderer';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 describe('Lobby', () => {
   it('should not render volume control', () => {
+    const tree = renderer.create(<Router><Lobby /></Router>).toJSON();
     const wrapper = shallow(<Router><Lobby /></Router>);
     expect(wrapper.find('#volume-slider')).toHaveLength(0);
+    expect(tree).toMatchSnapshot();
   })
 
   it('should render the lobby page', () => {
